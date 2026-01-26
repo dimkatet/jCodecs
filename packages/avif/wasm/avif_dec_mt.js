196185d2e377e96e422abf9068452b892dd7e2d0
Dmitriy Teterin <tet.dima.211@gmail.com>
2026-01-26 14:41:24 +0700

    Remove generated wasm modules

--- packages/avif/wasm/avif_dec_mt.js
+++
@@ -1,5 +0,0 @@
-// Multi-threaded decoder stub - not yet built
-// Build with BUILD_MT=ON to enable multi-threading
-export default function() {
-  throw new Error('Multi-threaded AVIF decoder not available. Build with BUILD_MT=ON.');
-}
